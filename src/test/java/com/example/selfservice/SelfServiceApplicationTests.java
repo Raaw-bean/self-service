88d6f5fdebd9b1b7ec15ad953e41a214718d6341
rawbean <robinbinsalam@outlook.com>
2024-05-19 11:12:15 +0600

    Initial template of the projcet ver 0.00

---
+++ src/test/java/com/example/selfservice/SelfServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.selfservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SelfServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
